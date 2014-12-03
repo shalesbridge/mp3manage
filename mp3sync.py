408a4816eb2ae7ef837b07b55103cfe9f4182b9b
shalesbridge <shalesbridge@users.noreply.github.com>
2014-12-03 15:11:56 -0500

    -Added mp3sync.py

---
+++ mp3sync.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import os
+import argparse
+
+def sync_local(src, dst):
+    for 
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.parse_args()
